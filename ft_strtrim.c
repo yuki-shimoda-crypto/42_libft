d2a5b32c6f18c55610537d7938b114037f07f23a
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-10-20 21:41:47 +0900

    fix strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,61 +6,35 @@
 /*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 11:14:10 by yshimoda          #+#    #+#             */
-/*   Updated: 2022/08/24 12:43:00 by yshimoda         ###   ########.fr       */
+/*   Updated: 2022/10/20 21:40:22 by yshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_find_trimmed(char const *c, char const *set)
-{
-	size_t	i;
-
-	i = 0;
-	while (set[i] != '\0')
-	{
-		if (set[i] == *c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	begin;
-	size_t	end;
-	size_t	i;
-	char	*ptr;
+	char const	*head;
+	char const	*tail;
+	char		*str;
 
-	begin = 0;
-	while (s1[begin] && ft_find_trimmed(&s1[begin], set))
-		begin++;
-	end = ft_strlen(s1);
-	while (begin < end && ft_find_trimmed(&s1[end - 1], set))
-		end--;
-	ptr = (char *)malloc(sizeof(char) * (end - begin));
-	if (ptr == NULL)
+	if (!s1)
 		return (NULL);
-	i = 0;
-	while (0 < end - begin)
-		ptr[i++] = s1[begin++];
-	ptr[i] = '\0';
-	return (ptr);
+	head = s1;
+	tail = s1 + ft_strlen(s1) - 1;
+	while (*head)
+	{
+		if (!ft_strchr(set, *head))
+			break ;
+		head++;
+	}
+	while (head < tail)
+	{
+		if (!ft_strchr(set, *tail))
+			break ;
+		tail--;
+	}
+	tail++;
+	str = ft_substr(head, 0, tail - head);
+	return (str);
 }
-
-// int	main(void)
-// {
-// 	printf("%s\t%s\n\n", ft_strtrim("hello world", "world"), "hello ");
-// 	printf("%s\t%s\n\n", ft_strtrim("hello world", "hello"), " world");
-// 	printf("%s\t%s\n\n", ft_strtrim("hello world", ""), "hello world");
-// 	printf("%s\t%s\n\n", ft_strtrim("", ""), "");
-// 	printf("%s\t%s\n\n", ft_strtrim("    hello world     ", " "),
-// 			"hello world");
-// 	printf("%s\n%s\n\n",
-// 			ft_strtrim(" \n\t\r    hello \n\t\r world     \r\t\n\t \r\n",
-// 				" \n\t\r"), "hello \n\t\r world");
-// 	printf("%s\t%s\n\n", ft_strtrim("hello world",
-// 				"abcdefghijklmnopqrstuvwxyz"), " ");
-// 	return (0);
-// }
