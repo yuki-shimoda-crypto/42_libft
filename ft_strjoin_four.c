7f8b60a3e2e917a7822638fb662d86e9fbe72e15
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-11-15 23:32:38 +0900

    add strjoin three add strjoin four

---
+++ ft_strjoin_four.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin_four.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 11:14:10 by yshimoda          #+#    #+#             */
+/*   Updated: 2022/11/15 23:31:35 by yshimoda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin_four(char const *s1, char const *s2, char const *s3, char const *s4)
+{
+	char	*ptr;
+	char	*save;
+
+	if (!s1 || !s2 || !s3 || !s4)
+		return (NULL);
+	ptr = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + ft_strlen(s3) + ft_strlen(s4) + 1);
+	if (!ptr)
+		return (NULL);
+	save = ptr;
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
+	while (*s3)
+		*ptr++ = *s3++;
+	while (*s4)
+		*ptr++ = *s4++;
+	*ptr = 0;
+	return (save);
+}
---
+++ ft_strjoin_three.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin_three.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 11:14:10 by yshimoda          #+#    #+#             */
+/*   Updated: 2022/11/15 23:30:14 by yshimoda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin_three(char const *s1, char const *s2, char const *s3)
+{
+	char	*ptr;
+	char	*save;
+
+	if (!s1 || !s2 || !s3)
+		return (NULL);
+	ptr = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + ft_strlen(s3) + 1);
+	if (!ptr)
+		return (NULL);
+	save = ptr;
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
+	while (*s3)
+		*ptr++ = *s3++;
+	*ptr = 0;
+	return (save);
+}
